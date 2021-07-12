501b0ddeff23b63b2d4de800f57081939e3837c5
riy4n_botz <86124000+riy4nb0tz@users.noreply.github.com>
2021-07-12 17:52:27 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '6282114499086', 'Benniismael', m)
-  this.sendContact(m.chat, '6282114499086', 'Benniismael', m)
+  this.sendContact(m.chat, '6283153406515', 'Riyan BOTZ', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
